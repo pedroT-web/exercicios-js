6ae725edf21b03ca27798471e5364d8b241c35d7
pedro <Pedrotaroco1@gmail.com>
2025-09-12 20:23:11 -0300

    Atividade 2 completa

---
+++ Exercico002/app.js
@@ -0,0 +1,19 @@
+let n1 = 10, n2 = 2
+
+//  console.log(n1+n2) //TODO + operador de adição
+document.getElementById("resultado_soma").innerText = n1 + n2 // entra na estrutura do dom(document), pesquisa um elemento pelo id(getElementById), e substitui o seu valor
+
+// console.log(n1-n2) //TODO - operador de subatração
+document.getElementById("resultado_subtracao").innerText = n1 - n2   
+
+// console.log(n1*n2) //TODO * operador de multiplicação
+document.getElementById("resultado_multiplicacao").innerText = n1 * n2
+
+// console.log(n1/n2) //TODO / operador de divisão
+document.getElementById("resultado_divisao").innerText = n1 / n2
+
+// console.log(n1%n2) //TODO % operador de módulo, pega o resto da divisão
+document.getElementById("resultado_modulo").innerText = n1 % n2
+
+// console.log(n1**n2) //TODO ** operador de exponenciação, pega um número e eleva a outro ex: 10²
+document.getElementById("resultado_exponente").innerText = n1**n2
