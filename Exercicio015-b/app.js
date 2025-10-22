d45a34f8e97ec5ca811abda66714a6d200438e9e
pedro <Pedrotaroco1@gmail.com>
2025-10-21 21:59:29 -0300

    Terminando a atividade 15

---
+++ Exercicio015-b/app.js
@@ -0,0 +1,54 @@
+let pokemon1 = {
+    id: 7,
+    nome: "charmander",
+    tipo: "fogo",
+    altura: 0.61,
+    peso: 8.5
+}
+
+let pokemon2 = {
+    id: 38,
+    nome: "Ninetails",
+    tipo: "fogo",
+    altura: 1.1,
+    peso: 19.9
+}
+
+console.log(pokemon2.nome)
+
+let salaDeAula = {
+    altura: 2.5,
+    largura: 5,
+    profundidade: 10,
+    corDaParede: "Branco Gelo",
+    numeroDeLampadas: 11,
+    numeroLugares: 25,
+    identificacao: "Sala 4"
+}
+
+console.log(salaDeAula.identificacao)
+
+let carro = {
+    marca: "Chevrolet",
+    modelo: "Astra",
+    ano: 1999,
+    cor: "Branco",
+    motor: 2.0,
+    quantidadeDePortas: 5,
+    tipo: "Sedan",
+    placa: "ABC-1010",
+    aro: 16
+}
+
+console.log(carro.marca)
+
+let comida = {
+    eSalgado: true,
+    origem: "Victor",
+    nome: "Strogonoff Doce",
+    pais: "Brasil",
+    ingredientes: "Açucar, picanha",
+    temperatura: "Gelado"
+}
+
+console.log(comida.origem)
---
+++ Exercicio015/app.js
@@ -0,0 +1,48 @@
+let contatos = []
+
+function fnSalvar() {
+    console.log(document.getElementById("nome").value)
+
+    let umContato = {
+        nome: document.getElementById("nome").value,
+        telefone: document.getElementById("telefone").value,
+        email: document.getElementById("email").value,
+        foto: document.getElementById("foto").value
+    }
+
+    contatos.push(umContato);
+    console.dir(contatos)
+
+    fnListar()
+}
+
+function fnListar() {
+    let dados = ""
+
+    contatos.forEach(function (umContato, i) {
+        dados += `
+        <div class="card mt-5 ms-5" style="max-width: 500px;">
+            <img src="${umContato.foto}" class="card-img-top imagens img-fluid mt-3 object-fit-cover">
+            <div class="card-body">
+                <p class="card-title">Nome: ${umContato.nome}</p>
+                <p class="card-title">Telefone: ${umContato.telefone} </p>
+                <p class="card-title">E-mail: ${umContato.email}</p>
+                <div class="text-end">
+                <button class="btn btn-danger">Deletar</button>
+                </div>
+            </div>
+        </div>        
+        `
+    })
+
+    document.getElementById("listaDeContatos").innerHTML = dados
+}
+
+function fnExcluir() {
+
+}
+
+//  addEventListener --> vai estar ouvindo o evento que acontece
+document.getElementById("btnSalvar").addEventListener('click', function () {
+    fnSalvar()
+})
